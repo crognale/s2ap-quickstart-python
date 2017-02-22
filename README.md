dd6811ca63d11d1aed3c2c648c5a9072cbb664a6
Sam Crognale <scrognale@google.com>
2017-02-22 09:36:08 -0800

    render index.html

--- README.md
+++ README.md
@@ -3,3 +3,4 @@ TODO format
 Dependencies: webapp2 (pip install webapp2)
 webob (pip install webob)
 paste (pip install paste)
+jinja2
--- main.py
+++ main.py
@@ -15,12 +15,22 @@ limitations under the License.
 """
 
 import config
+
 import webapp2
 from paste import httpserver
+import jinja2
+import os
+
+JINJA_ENVIRONMENT = jinja2.Environment(
+        loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+        extensions=['jinja2.ext.autoescape'],
+        autoescape=True)
 
 class MainPage(webapp2.RequestHandler):
     def get(self):
-        self.response.write('test')
+        template = JINJA_ENVIRONMENT.get_template('index.html')
+        template_values = {}
+        self.response.write(template.render(template_values))
 
 
 routes = [
